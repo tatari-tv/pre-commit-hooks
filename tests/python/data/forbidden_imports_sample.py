8659fe10b0e7a772b9dba3bf4770e7f1c0f56008
Russell Simco <rsty-instgrmpy@users.noreply.github.com>
2023-12-13 15:24:55 -0500

    fix imports in test

--- tests/python/data/forbidden_imports_sample.py
+++ tests/python/data/forbidden_imports_sample.py
@@ -1,10 +1,9 @@
+import enum as en
+from dataclasses import dataclass
 from datetime import date
 from datetime import datetime
 
-import pandas as pd
-import tuple
-
 do_something_with_date = date(2023, 1, 1)
 do_something_with_datetime = datetime(2023, 1, 1, 1, 1)
-do_something_with_tuple = tuple(1, 2)
-df = pd.DataFrame()
+do_something_with_dataclass = dataclass()
+do_something_with_enum = en.Enum('foo', 'bar')
--- tests/python/test_forbidden_imports.py
+++ tests/python/test_forbidden_imports.py
@@ -12,11 +12,11 @@ def test_bad_from_imports():
     assert check_imports(f'{dirname(__file__)}/data/forbidden_imports_sample.py', ['date', 'AnotherOKClass']) == 1
 
 
-# sample file imports tuple, which we will say is forbidden
+# sample file imports dataclass, which we will say is forbidden
 def test_bad_imports():
-    assert check_imports(f'{dirname(__file__)}/data/forbidden_imports_sample.py', ['tuple']) == 1
+    assert check_imports(f'{dirname(__file__)}/data/forbidden_imports_sample.py', ['dataclass']) == 1
 
 
-# sample file imports pandas aliased pd, which we will say is forbidden
+# sample file imports enum aliased en, which we will say is forbidden
 def test_bad_imports_aliased():
-    assert check_imports(f'{dirname(__file__)}/data/forbidden_imports_sample.py', ['pandas']) == 1
+    assert check_imports(f'{dirname(__file__)}/data/forbidden_imports_sample.py', ['enum']) == 1
