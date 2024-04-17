a4f905a83db033f9d0a3b661809557369db3fc18
Russell Simco <rsty-instgrmpy@users.noreply.github.com>
2024-04-17 11:39:22 -0400

    function checker with added functionality to ignore pre-commit hook with # tatari-noqa

---
+++ python_hooks/forbidden_function_calls.py
@@ -0,0 +1,63 @@
+import argparse
+import ast
+import sys
+
+from python_hooks.utills.ignore_check import FunctionCall
+from python_hooks.utills.ignore_check import ignore_check
+
+MESSAGE = "Flagged function call of {name} in {filename} @ line {line} column {col_offset} with {replacement}"
+
+
+def check_function_call(filename, forbidden_function: list[str], suggest_replace: list[str]):
+    check_accumulator = []
+    with open(filename) as file:
+        tree = ast.parse(file.read(), filename=filename)
+
+    for node in ast.walk(tree):
+        if isinstance(node, ast.Call):
+            if isinstance(node.func, ast.Attribute):
+                if node.func.attr in forbidden_function:
+                    index = forbidden_function.index(node.func.attr)
+                    # accumulate flagged functions
+                    check_accumulator.append(FunctionCall(node.func.attr, suggest_replace[index], node.lineno, node.col_offset))
+
+    # check if there's a tatari-noqa annotation in accumulator.
+    # If so, ignore the check and remove from accumulator.
+    # if not, print the flagged function calls
+    annotated_check_accumulator = ignore_check(filename, check_accumulator)
+
+    if len(annotated_check_accumulator) > 0:
+        for check in annotated_check_accumulator:
+            print(
+                MESSAGE.format(
+                    name=check.name, filename=filename, line=check.line, col_offset=check.col_offset, replacement=check.replacement
+                )
+            )
+        return 1
+    return 0  # Indicates success
+
+
+def validate_args(args):
+    if len(args.replacement_function_calls) != len(args.forbidden_function_calls):
+        raise ValueError("Number of replacement functions does not match the number of functions to check")
+
+
+def main(args):
+    return_code = 0
+
+    for file_path in args.file_list:
+        return_code |= check_function_call(file_path, args.forbidden_function_calls, args.replacement_function_calls)
+
+    return return_code
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Flags function calls for replacement with suggested replacement function')
+    parser.add_argument('--forbidden-function-calls', nargs='+', help='Names of the function calls to check', required=True)
+    parser.add_argument('--replacement-function-calls', nargs='+', help='Names of the function to replace in flagged calls', required=True)
+    parser.add_argument('file_list', nargs='+', help='List of files to check')  # provided by the pre-commit call
+    args = parser.parse_args()
+
+    validate_args(args)
+    return_code = main(args)
+    sys.exit(return_code)
---
+++ python_hooks/utills/ignore_check.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+
+NOQA = "tatari-noqa"
+
+
+@dataclass
+class FunctionCall:
+    name: str
+    replacement: str
+    line: int
+    col_offset: int
+
+
+def ignore_check(filename: str, check_fails: list[FunctionCall], noqa_string: str = NOQA) -> int:
+    """
+    Takes a filename, and a list of failed checks, and a noqa_string to check for in comments.
+    Checks each failed line for a noqa_string comment, and removes the check if it is found.
+    Returns the number of checks remaining.
+    """
+    # check_fails_no_ignores = []
+    with open(filename) as file:
+        failing_lines = [check.line for check in check_fails]
+        for i, line in enumerate(file):
+            if i + 1 in failing_lines:
+                for check in check_fails:
+                    if check.line == i + 1 and noqa_string in line:
+                        check_fails.remove(check)
+    return check_fails
