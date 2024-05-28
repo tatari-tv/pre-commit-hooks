4fd5fea3406d94110eba1837cda4b85686e6070f
keenanwells-tatari <keenan.wells@tatari.tv>
2024-05-28 16:09:01 -0700

    Update poetry-pkg-constraints

--- python_hooks/poetry_pkg_constraints.py
+++ python_hooks/poetry_pkg_constraints.py
@@ -1,14 +1,16 @@
 '''
 Validate dependency constraint formats for Python package repos.
 
-Currently supported formats are ^ and >=.
+Python version constraint currently supported formats are ^ and >=.
+Package version constraint currently supported format: >=
 '''
+from argparse import ArgumentParser
 from re import match
 
 from toml import load
 
 
-def validate_constraints(pyproject_path: str = 'pyproject.toml') -> int:
+def validate_constraints(ignore: list[str], pyproject_path: str = 'pyproject.toml') -> int:
     exit_status = 0
 
     # Load pyproject.toml as Python object
@@ -16,11 +18,6 @@ def validate_constraints(pyproject_path: str = 'pyproject.toml') -> int:
 
     # Iterate over all dependencies to get constraints
     for dep_name, dep_value in pyproject['tool']['poetry']['dependencies'].items():
-        # TODO: Remove this logic which only checks for python constraints
-        # Eventually want to validate all deps
-        if dep_name != 'python':
-            continue
-
         # Handle special cases of version definitions, e.g.:
         # dep = {"version" = "^1.0.0"}
         try:
@@ -28,14 +25,38 @@ def validate_constraints(pyproject_path: str = 'pyproject.toml') -> int:
         except TypeError:
             constraint = dep_value
 
-        # Regex match on supported formats
-        if not match(r'(\^|>=)', constraint):
-            print(f'INCORRECT FORMAT: {dep_name} = "{constraint}"')
-            exit_status = 1
-
-    print('All package constraints should use ^ (or less ideally >=) when defining versions\nFor example: python = "^3.10"')
+        if dep_name == 'python':
+            exit_status = validate_python_constraint(dep_name, constraint)
+        elif dep_name not in ignore:
+            if exit_status != 0:
+                validate_package_constraint(dep_name, constraint)
+            else:
+                exit_status = validate_package_constraint(dep_name, constraint)
     return exit_status
 
 
+def validate_python_constraint(dep_name: str, constraint: str) -> int:
+    # Regex match on supported formats
+    if not match(r'(\^|>=)', constraint):
+        print(f'INCORRECT FORMAT: {dep_name} = "{constraint}"')
+        print('Packages should use ^ (or less ideally >=) when defining python versions. For example: python = "^3.10"')
+        return 1
+
+    return 0
+
+
+def validate_package_constraint(dep_name: str, constraint: str) -> int:
+    # Regex match on supported formats
+    if not match(r'>=', constraint) or not (match(r'>=', constraint) and match(r'<=', constraint)):
+        print(f'INCORRECT FORMAT: {dep_name} = "{constraint}"')
+        print('Package constraints should use >= when defining versions. For example: tatari-pyspark = ">=1.0.14"')
+        return 1
+
+    return 0
+
+
 if __name__ == '__main__':
-    exit(validate_constraints())
+    parser = ArgumentParser(description="Validate dependency constraint formats for Python package repos.")
+    parser.add_argument("--ignore", nargs='+', default=[], help="List of packages to ignore constraint checking for.")
+    args = parser.parse_args()
+    exit(validate_constraints(args.ignore))
